79aa1e996f41759dbaf455e4a5dec7446325b202
Pedro Lucas Porcellis <pedrolucasporcellis@gmail.com>
2019-12-22 16:37:31 -0300

    Add Note's Title method

--- config/config.go
+++ config/config.go
@@ -1,36 +1,38 @@
 package config
 
 import (
-  "path"
-  "fmt"
-  "os/user"
+	"fmt"
+	"os/user"
+	"path"
 )
 
 type TConfig struct {
-  BasePath string
-  GitRemote string
+	BasePath  string
+	GitRemote string
 }
 
 func LoadBasicConfiguration() (*TConfig, error) {
-  var (
-    err error
-  )
+	var (
+		err error
+	)
 
-  currentUser, err := user.Current()
+	currentUser, err := user.Current()
 
-  if err != nil {
-    panic("Could not get the current user")
-  }
+	if err != nil {
+		panic("Could not get the current user")
+	}
 
-  base := path.Join(currentUser.HomeDir, "notes")
+	base := path.Join(currentUser.HomeDir, "notes")
 
-  fmt.Println(base)
+	fmt.Println(base)
 
-  config := &TConfig{BasePath: base, GitRemote: "origin"} 
+	config := &TConfig{BasePath: base, GitRemote: "origin"}
 
-  return config, err
+	fmt.Println(config)
+
+	return config, err
 }
 
 func Initialize() (*TConfig, error) {
-  return LoadBasicConfiguration()
+	return LoadBasicConfiguration()
 }
--- models/models.go
+++ models/models.go
@@ -1,6 +1,14 @@
 package models
 
+import (
+	"strings"
+)
+
 type Note struct {
-  Name string
-  Path string
+	Name string
+	Path string
+}
+
+func (note *Note) Title() string {
+	return strings.Split(note.Name, ".")[0]
 }
