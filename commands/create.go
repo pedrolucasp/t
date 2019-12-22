ae418aa2387201cd2b4e85c3562800d4fb9eea0b
Pedro Lucas Porcellis <pedrolucasporcellis@gmail.com>
2019-12-22 17:34:41 -0300

    Create a new Note

---
+++ commands/create.go
@@ -0,0 +1,26 @@
+package commands
+
+import (
+	"fmt"
+	"git.sr.ht/~porcellis/t/config"
+	"git.sr.ht/~porcellis/t/models"
+	"os"
+	"path"
+	"time"
+)
+
+func Create(config config.TConfig) (models.Note, error) {
+	var (
+		err  error
+		note models.Note
+	)
+
+	currentTime := time.Now()
+	noteName := currentTime.Format("2006-01-02T15:04:05Z07:00")
+	file, err := os.Create(path.Join(config.BasePath, fmt.Sprintf("%s.md", noteName)))
+
+	stat, _ := file.Stat()
+	note = models.Note{Name: stat.Name(), Path: path.Join(config.BasePath, stat.Name()), ModTime: stat.ModTime()}
+
+	return note, err
+}
